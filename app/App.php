5cb1a41ef9d951da7be568e5472ff1432967de7e
seif0912 <baddredineseif@gmail.com>
2022-06-01 23:02:04 +0100

    finiched

--- app/App.php
+++ app/App.php
@@ -3,3 +3,58 @@
 declare(strict_types = 1);
 
 // Your Code
+
+function getTransacionFiles(string $dirPath): array
+{
+    $files = [];
+    foreach(scandir($dirPath) as $file){
+        if (is_dir($file)){
+            continue;
+        }
+        $files[] = $dirPath . $file;
+        
+    }
+    return $files;
+}
+
+function getTransactions(string $fileName, ?callable $transationHandler = null): array {
+    if (! file_exists($fileName)){
+        trigger_error('File"'. $fileName . '" doesnot exist' , E_USER_ERROR);
+    }
+    $file = fopen($fileName, 'r');
+    fgetcsv($file);
+    $transations = [];
+    while(($transation = fgetcsv($file)) !== false){
+        if($transationHandler !== null){
+            $transation = $transationHandler($transation);
+        }
+        // $transations[] = extractTransaction($transation);
+        $transations[] = $transation;
+
+    }
+    return $transations;
+}
+
+function extractTransaction(array $transationRow): array {
+    [$date, $checkNumber, $description, $amount] = $transationRow;
+    $amount = (float) str_replace(['$', ','], '', $amount);
+    return [
+        'date' => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount' => $amount
+    ];
+}
+
+function calculateTotals(array $transactions): array {
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+    foreach($transactions as $transaction){
+        $totals['netTotal'] += $transaction['amount'];
+        if($transaction['amount'] >= 0){
+            $totals['totalIncome'] += $transaction['amount'];
+        }else{
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+    return $totals;
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,21 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+require APP_PATH . "App.php";
+require APP_PATH . 'helpers.php';
+
+$files = getTransacionFiles(FILES_PATH);
+
+$transactions = [];
+foreach($files as $file){
+    $transactions = array_merge($transactions, getTransactions($file, 'extractTransaction'));
+}
+
+$totals = calculateTotals($transactions);
+
+// var_dump($files);
+// // echo '<pre>';
+// print_r($transactions);
+// // echo '</pre>';
+
+require VIEWS_PATH . 'transactions.php';
